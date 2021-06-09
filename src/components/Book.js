783ee5bf09507c15f250b4a9922d37dd71a812d6
Henok <henatan99@gmail.com>
2021-06-09 19:57:08 +0300

    ADD Book component to render book object in a row

---
+++ src/components/Book.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const Book = (book) => {
+    <tr className="book">
+        <td className="book-id">book.id</td>
+        <td className="book-title">book.title</td>
+        <td class="book-category">book.category</td>
+    </tr>
+}
