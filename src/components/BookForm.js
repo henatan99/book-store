419085b39a55024cbefcc207ffee89d9aa5586d2
Henok <henatan99@gmail.com>
2021-06-09 20:28:48 +0300

    Add BookForm component which renders the form to fill Book info

---
+++ src/components/BookForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {connect} from "react-redux";
+
+class BookForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {title: "", category: ""};
+    }
+
+    updateInput = (title, category) => {
+        this.setState({title, category});
+    }
+
+    handleAddBook = () => {
+        
+    }
+    
+
+    render() {
+        const CATEGORIES = ["Action", "Biography", "History", 
+        "Horror", "Kids", "Learning", "Sci-Fi"];
+        return (
+            <form>
+                <input onChange={e => this.updateInput(e.target.value)} 
+                value={this.state.title} />
+                <select onChange={e => this.updateInput(e.target.value)} 
+                value={this.state.category} />
+                <button onClick={this.handleAddBook}>Add Book</button>
+            </form>
+        )
+    }
+}
