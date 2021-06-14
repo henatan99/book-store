69f4c0a53dd08567221f0034986384cc39c17235
Henok <henatan99@gmail.com>
2021-06-14 15:15:04 +0300

    Implement the categoryFilter component

---
+++ src/components/categoryFilter.js
@@ -0,0 +1,16 @@
+const categoryFilter = () => {
+    return (
+        <select>
+            <option>All</option>
+            <option>Action</option>
+            <option>Biography</option>
+            <option>History</option>
+            <option>Horror</option>
+            <option>Kids</option>
+            <option>Learning</option>
+            <option>Sci-Fi</option>
+        </select>
+    );
+}
+
+export default categoryFilter
